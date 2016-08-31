68262eca95c6af6b4f412271b6608c9b79b7919f
Ryan Andhaka & Toni <pair+ryanandhaka.tonychen09@hacktiv8.com>
2016-08-31 11:22:42 +0700

    chessboard&spreadsheet

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,16 @@
 //write your code here
+var chessBoard = [['Benteng Hitam','Kuda Hitam','Menteri Hitam','Raja Hitam',
+                  'Ratu Hitam','Menteri Hitam','Kuda Hitam','Benteng Hitam'],
+                  ['Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam',
+                  'Pion Hitam','Pion Hitam','Pion Hitam','Pion Hitam'],
+                  ['','','','','','','',''],
+                  ['','','','','','','',''],
+                  ['','','','','','','',''],
+                  ['','','','','','','',''],
+                  ['Pion Putih','Pion Putih','Pion Putih','Pion Putih',
+                  'Pion Putih','Pion Putih','Pion Putih','Pion Putih'],
+                  ['Benteng Putih','Kuda Putih','Menteri Putih','Raja Putih',
+                  'Ratu Putih','Menteri Putih','Kuda Putih','Benteng Putih'],
+                ]
+
+                console.log(chessBoard[7][0])
