b8cffcee50ed6708f04bcfeded63e660b063b793
Aji Lantang Mardika & Kun Anggiar Lanang <pair+ajilantang.kun7@hacktiv8.com>
2016-08-31 15:32:20 +0700

    tabel suka kami dan chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,29 @@
-//write your code here
+//tentukan nilai posisi dari tiap buah catur
+let papan = [
+    ["benteng hitam", "kuda hitam", "peluncur hitam", "ster hitam", "raja hitam", "peluncur hitam", "kuda hitam","benteng hitam" ],
+    ["pion hitam","pion hitam","pion hitam","pion hitam","pion hitam","pion hitam","pion hitam","pion hitam"],
+    ["kosong","kosong","kosong","kosong","kosong","kosong","kosong","kosong"],
+    ["kosong","kosong","kosong","kosong","kosong","kosong","kosong","kosong"],
+    ["kosong","kosong","kosong","kosong","kosong","kosong","kosong","kosong"],
+    ["kosong","kosong","kosong","kosong","kosong","kosong","kosong","kosong"],
+    ["pion putih","pion putih","pion putih","pion putih","pion putih","pion putih","pion putih","pion putih"],
+    ["benteng putih","kuda putih","peluncur putih","ster putih","raja putih","peluncur putih","kuda putih","benteng putih"]
+  ];
+
+// console.log(papan[0][1]+[6][0]);
+// //
+// let pionPutih = []//posisi pionputih
+// for  (a ass i = 0; i < 8; i++){
+//   for (var j = 0; j < 8; i++){
+//     if (i+j  ) {
+//
+//     }
+//   }
+//   }
+for (var i = 0; i < 8; i++) {
+  for (var j = 0; j < 8; j++) {
+    console.log(papan[i][j]+"="+"("+i+","+j+")");
+    }
+    //
+}
+// console.log (papan[3][7]); //true
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,24 @@
 //write your code here
+let number = ["nomor",1,2,3,4]
+let id = ["id",928,723,237,237]
+let nama = ["nama","aji","ikun","sanjaya","hanif"]
+let Position = ["POSITION","pointGuard","center","ShootingGuard", "smallForward" ]
+let pointpergame = ["pointpergame", [19, 0, 11, 22, 0], [0, 30, 16, 0, 25], [18, 29, 26, 31, 19], [11, 0, 23, 17, 0]]
+let table = [number,id, nama, Position,pointpergame]
+for (var i = 0; i < 5; i++) {
+    console.log(table[0][i]+" "+table[1][i]+" "+table[2][i]+" "+table[3][i]+" "+table[4][i])
+
+}
+// table.screens.push({'name':'screen':'width':450});
+// localStorage.setItem("session", JSON.stringify(session));
+//
+// var restoredSession = JSON.parse(localStorage.getItem("session"));
+//
+// console.log(restoredSession);
+// let table = ["Joeschmo", "Ken Buckets", "Harvey Kay",
+//         "Sally Talls", "Mo DiBoux"],
+//         []
+// console.log (table[3] [2] == "shooting Guard");
+//
+// console.log(JSON.stringify(table[1][3])) == JSON.stringify([14,
+//   32, 7, 0, 23]);
