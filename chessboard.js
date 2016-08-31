ce595d9724b5cf804fed6bd52b6b149737c026bb
Ari Adiprana & Tevin <pair+ariadiprana.tevinstein@hacktiv8.com>
2016-08-31 10:36:20 +0700

    release 0

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,13 @@
 //write your code here
+var chessboard = [
+  ['benteng hitam','kuda hitam','gajah hitam','ratu hitam','raja hitam','gajah hitam','kuda hitam','benteng hitam'],
+  ['pion hitam','pion hitam','pion hitam','pion hitam','pion hitam','pion hitam','pion hitam','pion hitam'],
+  ['','','','','','','','',],
+  ['','','','','','','','',],
+  ['','','','','','','','',],
+  ['','','','','','','','',],
+  ['pion putih','pion putih','pion putih','pion putih','pion putih','pion putih','pion putih','pion putih'],
+  ['benteng putih','kuda putih','gajah putih','ratu putih','raja putih','gajah putih','kuda putih','benteng putih']
+]
+
+console.log(chessboard[0][1]=="benteng hitam");
