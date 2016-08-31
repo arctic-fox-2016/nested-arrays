fcbaf4fdd9bbfb352f8c624a8059cd4990fe670c
Ahmad Ahmadi & Peter Raswono <pair+ahmadahmadi14.praswono@hacktiv8.com>
2016-08-31 14:17:23 +0700

    peter & Ahmad

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,44 @@
 //write your code here
+var chessBoard = [];
+
+function startChessBoard() {
+  for(var row = 0; row < 8; row++) {
+    chessBoard[row] = [];
+  }
+
+  for(var row = 0; row < 8; row++) {
+    for(var col = 0; col < 8; col++) {
+    chessBoard = fillChessBoard(chessBoard, row, col);
+    }
+  }
+}
+
+function fillChessBoard(chessBoard, row, col) {
+  if (row == 1 || row == 6){
+      chessBoard[row][col] = "Pion";
+  }else if(row == 0 && col == 0 || row == 0 && col == 7 || row == 7 && col == 0 || row == 7 && col == 7){
+      chessBoard[row][col] = "Benteng";
+  }else if(row == 0 && col == 1 || row == 0 && col == 6 || row == 7 && col == 1 || row == 7 && col == 6){
+      chessBoard[row][col] = "Kuda";
+  }else if(row == 0 && col == 2 || row == 0 && col == 5 || row == 7 && col == 2 || row == 7 && col == 5){
+      chessBoard[row][col] = "Gajah";
+  }else if(row == 0 && col == 3 || row == 7 && col == 3){
+      chessBoard[row][col] = "Raja";
+  }else if(row == 0 && col == 4 || row == 7 && col == 4){
+      chessBoard[row][col] = "Ratu";
+  } else {
+      chessBoard[row][col] = "";
+  }
+
+  if (row == 0 ||  row == 1){
+    chessBoard[row][col] += " Hitam";
+  }
+  else if (row == 6 || row == 7){
+    chessBoard[row][col] += " Putih";
+  }
+
+    return chessBoard;
+}
+
+startChessBoard();
+console.log(chessBoard[7][0] == "Benteng Putih");
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,16 @@
 //write your code here
+var playerData = [["Number", "Name", "Position", "Points per Game"]];
+
+// Insert player data into table
+function createData() {
+
+  playerData.push([12, "Joe Schmo", "Center", [14, 32, 7, 0, 23]]);
+  playerData.push([9, "Ken Buckets", "Point Guard", [19, 0, 11, 22, 0]]);
+  playerData.push([31, "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]]);
+  playerData.push([18, "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]]);
+  playerData.push([22, "Mo Diboux", "Small Forward", [11, 0, 23, 17, 0]]);
+}
+
+createData();
+console.log(playerData[3][2] == "Shooting Guard");
+console.log(JSON.stringify(playerData[1][3]) == JSON.stringify([14, 32, 7, 0, 23]));
