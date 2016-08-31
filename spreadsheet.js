f9ccc0122f4d5ff9d27c11855ff450559f3fe329
Andrew Tandiawan & Elkana Lumbantobing <pair+atandiawan.elkana-ltobing@hacktiv8.com>
2016-08-31 15:21:10 +0700

    Tambah Release 1

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,10 @@
 //write your code here
+let basket = [
+  ["15","Elkana","Point Guard",[20,30,10,20,25]],
+  ["20","Andrew","Shooting Guard", [20,10,15,20,15]],
+  ["3", "Haida", "Small Forward", [20,20,20,20,30]],
+  ["41", "Riz","Power Forward", [20,40,20,10,20]],
+  ["55", "Rub", "Center", [10,50,30,40,20]]
+]
+
+console.log(basket[3][2])
