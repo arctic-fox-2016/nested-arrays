056e81344502f6ab21370defad47738b9b56918d
ismanuri & Liliwibi <pair+ismanuri.lilianti@hacktiv8.com>
2016-08-31 10:58:08 +0700

    nested-arrays selesai

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,12 @@
 //write your code here
+let papanCatur = [
+  ["hitam_benteng","hitam_kuda","hitam_gajah","hitam_ratu","hitam_raja","hitam_gajah","hitam_kuda","hitam_benteng"],
+  ["hitam_pion","hitam_pion","hitam_pion","hitam_pion","hitam_pion","hitam_pion","hitam_pion","hitam_pion"],
+  ["","","","","","","",""],
+  ["","","","","","","",""],
+  ["","","","","","","",""],
+  ["","","","","","","",""],
+  ["putih_pion","putih_pion","putih_pion","putih_pion","putih_pion","putih_pion","putih_pion","putih_pion"],
+  ["putih_benteng","putih_kuda","putih_gajah","putih_ratu","putih_raja","putih_gajah","putih_kuda","putih_benteng"]
+]
+console.log(papanCatur[7][0] ==  "putih_benteng")
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,11 @@
 //write your code here
+let table = [
+  ["Number","Name","Position","Point per Game"],
+  [12,"Joe Schmo","Center",[14,32,7,0,23]],
+  [9,"Ken","Point Guard",[19,0,16,22,0]],
+  [31,"Harvey","Shooting Guard",[0,30,16,0,25]],
+  [18,"Sally","Power Forward",[18,29,26,31,19]],
+  [22,"Mo","Small Forward",[11,0,23,17,0]]
+]
+console.log(table[3][2] == "Shooting Guard");
+console.log(JSON.stringify(table[1][3]) == JSON.stringify([14,32,7,0,23]));
